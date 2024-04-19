cbe4dfa9c7b693046521ef97cc8f10be92b4b0a1
wesleygrignani <wesley.grignani@edu.univali.br>
2024-04-19 16:04:42 -0300

    Update encoding method

--- hdl/slave/backend/injection/backend_slave_packetizer_control.vhd
+++ hdl/slave/backend/injection/backend_slave_packetizer_control.vhd
@@ -33,9 +33,9 @@ entity backend_slave_packetizer_control is
 end backend_slave_packetizer_control;
 
 architecture rtl of backend_slave_packetizer_control is
-    type t_STATE is (S_IDLE, S_H_DEST, S_H_SRC, S_H_INTERFACE, S_PAYLOAD, S_TRAILER);
-    signal r_STATE: t_STATE;
-    signal r_NEXT_STATE: t_STATE;
+
+    signal state_w_r    : std_logic_vector(2 downto 0);
+    signal next_state_w : std_logic_vector(2 downto 0);
 
 begin
     ---------------------------------------------------------------------------------------------
@@ -43,9 +43,9 @@ begin
     process (all)
     begin
         if (ARESETn = '0') then
-            r_STATE <= S_IDLE;
+            state_w_r <= "000";
         elsif (rising_edge(ACLK)) then
-            r_STATE <= r_NEXT_STATE;
+            state_w_r <= next_state_w;
         end if;
     end process;
 
@@ -53,60 +53,61 @@ begin
     -- State machine.
     process (all)
     begin
-        case r_STATE is
-            when S_IDLE => if (i_HAS_REQUEST_PACKET = '1' and i_VALID_SEND_DATA = '1' and i_WRITE_OK_BUFFER = '1') then r_NEXT_STATE <= S_H_DEST; else r_NEXT_STATE <= S_IDLE; end if;
+        case state_w_r is
+            when "000" => if (i_HAS_REQUEST_PACKET = '1' and i_VALID_SEND_DATA = '1' and i_WRITE_OK_BUFFER = '1') then next_state_w <= "001"; else next_state_w <= "000"; end if;
 
-            when S_H_DEST => if (i_WRITE_OK_BUFFER = '1') then r_NEXT_STATE <= S_H_SRC; else r_NEXT_STATE <= S_H_DEST; end if;
+            when "001" => if (i_WRITE_OK_BUFFER = '1') then next_state_w <= "010"; else next_state_w <= "001"; end if;
 
-            when S_H_SRC => if (i_WRITE_OK_BUFFER = '1') then r_NEXT_STATE <= S_H_INTERFACE; else r_NEXT_STATE <= S_H_SRC; end if;
+            when "010" => if (i_WRITE_OK_BUFFER = '1') then next_state_w <= "011"; else next_state_w <= "010"; end if;
 
-            when S_H_INTERFACE => if (i_WRITE_OK_BUFFER = '1') then
-                                      if (i_OPC_SEND = '0') then
-                                          r_NEXT_STATE <= S_TRAILER; -- Write packet. Next flit trailer.
-                                      else
-                                          r_NEXT_STATE <= S_PAYLOAD; -- Read packet. Next flit is payload.
-                                      end if;
-                                  else
-                                      r_NEXT_STATE <= S_H_INTERFACE;
-                                  end if;
+            when "011" => if (i_WRITE_OK_BUFFER = '1') then
+                            if (i_OPC_SEND = '0') then
+                              next_state_w <= "101"; -- Write packet. Next flit trailer.
+                            else
+                              next_state_w <= "100"; -- Read packet. Next flit is payload.
+                            end if;
+                          else
+                            next_state_w <= "011";
+                          end if;
 
-            when S_PAYLOAD => if (i_VALID_SEND_DATA = '1' and i_WRITE_OK_BUFFER = '1' and i_LAST_SEND_DATA = '1') then
-                                  r_NEXT_STATE <= S_TRAILER;
-                              else
-                                  r_NEXT_STATE <= S_PAYLOAD;
-                              end if;
+            when "100" => if (i_VALID_SEND_DATA = '1' and i_WRITE_OK_BUFFER = '1' and i_LAST_SEND_DATA = '1') then
+                            next_state_w <= "101";
+                          else
+                            next_state_w <= "100";
+                          end if;
 
-            when S_TRAILER => if (i_WRITE_OK_BUFFER = '1') then r_NEXT_STATE <= S_IDLE; else r_NEXT_STATE <= S_TRAILER; end if;
+            when "101" => if (i_WRITE_OK_BUFFER = '1') then next_state_w <= "000"; else next_state_w <= "101"; end if;
 
-            when others => r_NEXT_STATE <= S_IDLE;
+            when others => next_state_w <= "000";
         end case;
     end process;
 
     ---------------------------------------------------------------------------------------------
     -- Output values.
-    o_READY_SEND_DATA <= '1' when (r_STATE = S_IDLE and i_OPC_SEND = '0' and i_HAS_REQUEST_PACKET = '1') or -- For writes.
-                                  (r_STATE = S_PAYLOAD and i_WRITE_OK_BUFFER = '1') -- For reads.
+    o_READY_SEND_DATA <= '1' when (state_w_r = "000" and i_OPC_SEND = '0' and i_HAS_REQUEST_PACKET = '1') or -- For writes.
+                                  (state_w_r = "100" and i_WRITE_OK_BUFFER = '1') -- For reads.
                                   else '0';
 
-    o_FLIT_SELECTOR <= "000" when (r_STATE = S_H_DEST) else
-                       "001" when (r_STATE = S_H_SRC) else
-                       "010" when (r_STATE = S_H_INTERFACE) else
-                       "011" when (r_STATE = S_PAYLOAD) else
-                       "100" when (r_STATE = S_TRAILER) else
+    o_FLIT_SELECTOR <= "000" when (state_w_r = "001") else
+                       "001" when (state_w_r = "010") else
+                       "010" when (state_w_r = "011") else
+                       "011" when (state_w_r = "100") else
+                       "100" when (state_w_r = "101") else
                        "111";
 
-    o_HAS_FINISHED_RESPONSE <= '1' when (r_STATE = S_TRAILER and i_WRITE_OK_BUFFER = '1') else '0';
+    o_HAS_FINISHED_RESPONSE <= '1' when (state_w_r = "101" and i_WRITE_OK_BUFFER = '1') else '0';
 
-    o_WRITE_BUFFER <= '1' when (r_STATE = S_H_DEST) or
-                               (r_STATE = S_H_SRC) or
-                               (r_STATE = S_H_INTERFACE) or
-                               (r_STATE = S_PAYLOAD and i_VALID_SEND_DATA = '1') or
-                               (r_STATE = S_TRAILER) else '0';
+    o_WRITE_BUFFER <= '1' when (state_w_r = "001") or
+                               (state_w_r = "010") or
+                               (state_w_r = "011") or
+                               (state_w_r = "100" and i_VALID_SEND_DATA = '1') or
+                               (state_w_r = "101") else '0';
 
-    o_ADD <= '1' when ((r_STATE = S_H_DEST) or
-                       (r_STATE = S_H_SRC) or
-                       (r_STATE = S_H_INTERFACE) or
-                       (r_STATE = S_PAYLOAD and i_VALID_SEND_DATA = '1')) and i_WRITE_OK_BUFFER = '1' else '0';
+    o_ADD <= '1' when ((state_w_r = "001") or
+                       (state_w_r = "010") or
+                       (state_w_r = "011") or
+                       (state_w_r = "100" and i_VALID_SEND_DATA = '1')) and i_WRITE_OK_BUFFER = '1' else '0';
 
-    o_INTEGRITY_RESETn <= '0' when (r_STATE = S_IDLE) else '1';
+    o_INTEGRITY_RESETn <= '0' when (state_w_r = "000") else '1';
+
 end rtl;
