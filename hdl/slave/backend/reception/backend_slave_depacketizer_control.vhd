3809e53b785622fe999074595d7df447f947485b
wesleygrignani <wesley.grignani@edu.univali.br>
2024-04-20 00:09:04 -0300

    Update encoding method

--- hdl/slave/backend/reception/backend_slave_depacketizer_control.vhd
+++ hdl/slave/backend/reception/backend_slave_depacketizer_control.vhd
@@ -43,10 +43,9 @@ entity backend_slave_depacketizer_control is
 end backend_slave_depacketizer_control;
 
 architecture rtl of backend_slave_depacketizer_control is
-    type t_STATE is (S_H_DEST, S_H_SRC, S_H_INTERFACE, S_H_ADDRESS,
-                     S_WRITE_REQUEST, S_WRITE_REQUEST_PAYLOAD, S_READ_REQUEST, S_TRAILER, S_WAIT_RESPONSE);
-    signal r_STATE: t_STATE;
-    signal r_NEXT_STATE: t_STATE;
+    
+    signal state_w_r    : std_logic_vector(3 downto 0);
+    signal next_state_w : std_logic_vector(3 downto 0);
 
     signal r_PAYLOAD_COUNTER: unsigned(7 downto 0) := to_unsigned(255, 8);
     signal r_SET_PAYLOAD_COUNTER: std_logic := '0';
@@ -58,9 +57,9 @@ begin
     process (all)
     begin
         if (ARESETn = '0') then
-            r_STATE <= S_H_DEST;
+            state_w_r <= "0000";
         elsif (rising_edge(ACLK)) then
-            r_STATE <= r_NEXT_STATE;
+            state_w_r <= next_state_w;
         end if;
     end process;
 
@@ -68,43 +67,45 @@ begin
     -- State machine.
     process (all)
     begin
-        case r_STATE is
-            when S_H_DEST => if (i_READ_OK_BUFFER = '1') then r_NEXT_STATE <= S_H_SRC; else r_NEXT_STATE <= S_H_DEST; end if;
+        case state_w_r is
+            when "0000" => if (i_READ_OK_BUFFER = '1') then next_state_w <= "0001"; else next_state_w <= "0000"; end if;
 
-            when S_H_SRC  => if (i_READ_OK_BUFFER = '1') then r_NEXT_STATE <= S_H_INTERFACE; else r_NEXT_STATE <= S_H_SRC; end if;
+            when "0001"  => if (i_READ_OK_BUFFER = '1') then next_state_w <= "0010"; else next_state_w <= "0001"; end if;
 
-            when S_H_INTERFACE => if (i_READ_OK_BUFFER = '1') then r_NEXT_STATE <= S_H_ADDRESS; else r_NEXT_STATE <= S_H_INTERFACE; end if;
+            when "0010" => if (i_READ_OK_BUFFER = '1') then next_state_w <= "0011"; else next_state_w <= "0010"; end if;
 
-            when S_H_ADDRESS => if (i_READ_OK_BUFFER = '1') then
+            when "0011" => if (i_READ_OK_BUFFER = '1') then
                                     if (i_H_INTERFACE(1) = '0') then
                                         -- Write request.
-                                        r_NEXT_STATE <= S_WRITE_REQUEST;
+                                        next_state_w <= "0100";
                                     else
                                         -- Read request. Next flit is trailer.
-                                        r_NEXT_STATE <= S_READ_REQUEST;
+                                        next_state_w <= "0110";
                                     end if;
                                 else
-                                    r_NEXT_STATE <= S_H_ADDRESS;
+                                    next_state_w <= "0011";
                                 end if;
 
-            when S_WRITE_REQUEST => if (i_READY_RECEIVE_PACKET = '1') then r_NEXT_STATE <= S_WRITE_REQUEST_PAYLOAD; else r_NEXT_STATE <= S_WRITE_REQUEST; end if;
+            when "0100" => if (i_READY_RECEIVE_PACKET = '1') then next_state_w <= "0101"; else next_state_w <= "0100"; end if;
 
-            when S_WRITE_REQUEST_PAYLOAD => if (r_PAYLOAD_COUNTER = to_unsigned(0, 8) and i_READY_RECEIVE_DATA = '1' and i_READ_OK_BUFFER = '1') then
-                                                r_NEXT_STATE <= S_TRAILER;
-                                            else
-                                                r_NEXT_STATE <= S_WRITE_REQUEST_PAYLOAD;
-                                            end if;
+            when "0101" => if (r_PAYLOAD_COUNTER = to_unsigned(0, 8) and i_READY_RECEIVE_DATA = '1' and i_READ_OK_BUFFER = '1') then
+                             next_state_w <= "0111";
+                           else
+                             next_state_w <= "0101";
+                           end if;
 
-            when S_READ_REQUEST => if (i_READY_RECEIVE_PACKET = '1') then r_NEXT_STATE <= S_TRAILER; else r_NEXT_STATE <= S_READ_REQUEST; end if;
+            when "0110" => if (i_READY_RECEIVE_PACKET = '1') then next_state_w <= "0111"; else next_state_w <= "0110"; end if;
 
-            when S_TRAILER => if (i_READ_OK_BUFFER = '1') then r_NEXT_STATE <= S_WAIT_RESPONSE; else r_NEXT_STATE <= S_TRAILER; end if;
+            when "0111" => if (i_READ_OK_BUFFER = '1') then next_state_w <= "1000"; else next_state_w <= "0111"; end if;
 
-            when S_WAIT_RESPONSE => if (i_HAS_FINISHED_RESPONSE = '1') then r_NEXT_STATE <= S_H_DEST; else r_NEXT_STATE <= S_WAIT_RESPONSE; end if;
+            when "1000" => if (i_HAS_FINISHED_RESPONSE = '1') then next_state_w <= "0000"; else next_state_w <= "1000"; end if;
+
         end case;
     end process;
 
     ---------------------------------------------------------------------------------------------
     -- Payload counter.
+    -- TODO: Apply ECC
     process (all)
     begin
         if (ARESETn = '0') then
@@ -120,41 +121,45 @@ begin
 
     ---------------------------------------------------------------------------------------------
     -- Internal signals.
-    r_SET_PAYLOAD_COUNTER      <= '1' when (r_STATE = S_H_ADDRESS) else '0';
-    r_SUBTRACT_PAYLOAD_COUNTER <= '1' when (r_STATE = S_WRITE_REQUEST_PAYLOAD and i_READ_OK_BUFFER = '1' and i_READY_RECEIVE_DATA = '1') else '0';
+    r_SET_PAYLOAD_COUNTER      <= '1' when (state_w_r = "0011") else '0';
+    r_SUBTRACT_PAYLOAD_COUNTER <= '1' when (state_w_r = "0101" and i_READ_OK_BUFFER = '1' and i_READY_RECEIVE_DATA = '1') else '0';
 
     ---------------------------------------------------------------------------------------------
     -- Output values.
-    o_READ_BUFFER <= '1' when (r_STATE = S_H_DEST) or
-                              (r_STATE = S_H_SRC) or
-                              (r_STATE = S_H_INTERFACE) or
-                              (r_STATE = S_H_ADDRESS) or
-                              (r_STATE = S_WRITE_REQUEST_PAYLOAD and i_READY_RECEIVE_DATA = '1') or
-                              (r_STATE = S_TRAILER)
+    o_READ_BUFFER <= '1' when (state_w_r = "0000") or
+                              (state_w_r = "0001") or
+                              (state_w_r = "0010") or
+                              (state_w_r = "0011") or
+                              (state_w_r = "0101" and i_READY_RECEIVE_DATA = '1') or
+                              (state_w_r = "0111")
                               else '0';
 
-    o_VALID_RECEIVE_PACKET <= '1' when (r_STATE = S_WRITE_REQUEST) or
-                                       (r_STATE = S_READ_REQUEST)
+    o_VALID_RECEIVE_PACKET <= '1' when (state_w_r = "0100") or
+                                       (state_w_r = "0110")
                                        else '0';
 
-    o_VALID_RECEIVE_DATA <= '1' when (r_STATE = S_WRITE_REQUEST_PAYLOAD and i_READ_OK_BUFFER = '1') or
-                                     (r_STATE = S_READ_REQUEST)
+    o_VALID_RECEIVE_DATA <= '1' when (state_w_r = "0101" and i_READ_OK_BUFFER = '1') or
+                                     (state_w_r = "0110")
                                      else '0';
-    o_LAST_RECEIVE_DATA  <= '1' when (r_STATE = S_WRITE_REQUEST_PAYLOAD and i_READ_OK_BUFFER = '1' and r_PAYLOAD_COUNTER = to_unsigned(0, 8)) else '0';
 
-    o_HAS_REQUEST_PACKET <= '1' when (r_STATE = S_WAIT_RESPONSE) else '0';
+    o_LAST_RECEIVE_DATA  <= '1' when (state_w_r = "0101" and i_READ_OK_BUFFER = '1' and r_PAYLOAD_COUNTER = to_unsigned(0, 8)) else '0';
 
-    o_WRITE_H_SRC_REG       <= '1' when (r_STATE = S_H_SRC) else '0';
-    o_WRITE_H_INTERFACE_REG <= '1' when (r_STATE = S_H_INTERFACE) else '0';
-    o_WRITE_H_ADDRESS_REG   <= '1' when (r_STATE = S_H_ADDRESS)  else '0';
+    o_HAS_REQUEST_PACKET <= '1' when (state_w_r = "1000") else '0';
 
-    o_ADD <= '1' when ((r_STATE = S_H_DEST) or
-                       (r_STATE = S_H_SRC) or
-                       (r_STATE = S_H_INTERFACE) or
-                       (r_STATE = S_H_ADDRESS) or
-                       (r_STATE = S_WRITE_REQUEST_PAYLOAD and i_READY_RECEIVE_DATA = '1')) and i_READ_OK_BUFFER = '1' else '0';
+    o_WRITE_H_SRC_REG       <= '1' when (state_w_r = "0001") else '0';
 
-    o_COMPARE <= '1' when (r_STATE = S_TRAILER and i_READ_OK_BUFFER = '1') else '0';
+    o_WRITE_H_INTERFACE_REG <= '1' when (state_w_r = "0010") else '0';
 
-    o_INTEGRITY_RESETn <= '0' when (r_STATE = S_H_DEST and r_NEXT_STATE = S_H_DEST) else '1';
+    o_WRITE_H_ADDRESS_REG   <= '1' when (state_w_r = "0011")  else '0';
+
+    o_ADD <= '1' when ((state_w_r = "0000") or
+                       (state_w_r = "0001") or
+                       (state_w_r = "0010") or
+                       (state_w_r = "0011") or
+                       (state_w_r = "0101" and i_READY_RECEIVE_DATA = '1')) and i_READ_OK_BUFFER = '1' else '0';
+
+    o_COMPARE <= '1' when (state_w_r = "0111" and i_READ_OK_BUFFER = '1') else '0';
+
+    o_INTEGRITY_RESETn <= '0' when (state_w_r = "0000" and next_state_w = "0000") else '1';
+
 end rtl;
