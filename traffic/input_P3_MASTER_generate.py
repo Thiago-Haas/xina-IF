a56675e10632a14c7dc3bbdae8b550614f693e25
Thiago-Haas <thiagorausch@hotmail.com>
2024-04-22 18:22:07 -0300

    changed traffic generation

--- traffic/input_P3_MASTER_generate.py
+++ traffic/input_P3_MASTER_generate.py
@@ -9,10 +9,11 @@
 #constant type_w: std_logic := '1';
 #signal payload1_w: std_logic_vector(data_width_p downto 0) := "0" & "1010101010101010" & "1010101010101010";
 #signal trailer_w : std_logic_vector(data_width_p downto 0) := "1" & "1000100010001010" & "0000100100110010";
+#signal address_w : std_logic_vector(data_width_p downto 0) := "0" & "1101110111011101" & "1101110111011101";
 #signal data_out_w: std_logic_vector(data_width_p downto 0);
 # Entry parameters
 import random
-SEED = 123  # Seed for random number generation
+SEED = 7572865  # Seed for random number generation
 NUM_LINES = 1000  # Number of traffic lines to generate
 
 def generate_traffic(seed, num_lines):
@@ -26,11 +27,12 @@ def generate_traffic(seed, num_lines):
 def write_to_file(traffic_lines):
     with open("input_P3_MASTER_traffic.txt", "w") as f:
         for line in traffic_lines:
-            f.write("1"+"00000000000000000000000000000000" + '\n')#header_dest_w
-            f.write("0"+"00000000000000010000000000000000" + '\n')#header_src_w
-            f.write("0"+"000000000000"+"00001"+"00000001"+"01"+"010"+"1"+"1" + '\n')#header_interface_w "0" "000000000000" & id_w & len_w & burst_w & status_w & opc_w & type_w;
-            f.write("1" + line + '\n')#paload
-            f.write("1"+"00000000000000000000000000000000" + '\n')#trailer_w
+            #f.write("1"+"00000000000000000000000000000000" + '\n')#header_dest_w
+            #f.write("0"+"00000000000000010000000000000000" + '\n')#header_src_w
+            #f.write("0"+"000000000000"+"00001"+"00000001"+"01"+"010"+"1"+"1" + '\n')#header_interface_w "0" "000000000000" & id_w & len_w & burst_w & status_w & opc_w & type_w;
+            #f.write("0"+ "1111111111111111" + "1111111111111111" + '\n')
+            f.write("0" + line + '\n')#paload
+            #f.write("1"+"00000000000000000000000000000000" + '\n')#trailer_w
 
 def main():
     traffic_lines = generate_traffic(SEED, NUM_LINES)
