c7f54236953205c942deaa1a98aa21facb9ee6a2
THIAGO HAAS RAUSCH <thiagorausch@hotmail.com>
2026-03-11 13:50:07 +0100

    Update xina_ni_ft_pkg.vhd

--- rtl/NI_obs/xina_ni_ft_pkg.vhd
+++ rtl/NI_obs/xina_ni_ft_pkg.vhd
@@ -57,13 +57,13 @@ package xina_ni_ft_pkg is
   constant c_ENABLE_TG_CTRL_TMR              : boolean := true;
   constant c_ENABLE_TG_HAMMING_PROTECTION    : boolean := true;
   constant c_ENABLE_TG_HAMMING_DOUBLE_DETECT : boolean := true;
-  constant c_ENABLE_TG_HAMMING_INJECT_ERROR  : boolean := true;
+  constant c_ENABLE_TG_HAMMING_INJECT_ERROR  : boolean := true; -- INJECTION
 
   -- TM.
   constant c_ENABLE_TM_CTRL_TMR              : boolean := true;
   constant c_ENABLE_TM_HAMMING_PROTECTION    : boolean := true;
   constant c_ENABLE_TM_HAMMING_DOUBLE_DETECT : boolean := true;
-  constant c_ENABLE_TM_HAMMING_INJECT_ERROR  : boolean := true;
+  constant c_ENABLE_TM_HAMMING_INJECT_ERROR  : boolean := true; -- INJECTION
   constant c_ENABLE_TM_TXN_COUNTER_HAMMING   : boolean := true;
   constant c_TM_TRANSACTION_COUNTER_WIDTH     : natural := 24;
   -- Number of TM completed packets between periodic UART reports.
@@ -73,7 +73,7 @@ package xina_ni_ft_pkg is
   constant c_ENABLE_LB_CTRL_TMR              : boolean := true;
   constant c_ENABLE_LB_HAMMING_PROTECTION    : boolean := true;
   constant c_ENABLE_LB_HAMMING_DOUBLE_DETECT : boolean := true;
-  constant c_ENABLE_LB_HAMMING_INJECT_ERROR  : boolean := true;
+  constant c_ENABLE_LB_HAMMING_INJECT_ERROR  : boolean := true; -- INJECTION
 
   -- Hamming FIFO control-state TMR (stage_valid + fifo_count in buffer_fifo_ham).
   constant c_ENABLE_HAM_FIFO_CTRL_TMR : boolean := true;
